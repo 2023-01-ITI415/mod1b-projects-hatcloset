faa9d6d695bd9ea1fe008a5ef9151196de2cfd25
Tankakuka <officialtankakuka@gmail.com>
2023-02-01 23:44:02 -0500

    Store HighScore between sessions. Complete Prototype

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -13,6 +13,14 @@ public class HighScore : MonoBehaviour
     void Awake()
     {
         _UI_TEXT = GetComponent<Text>();
+
+        // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            SCORE = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", SCORE);
     }
     
     static public int SCORE
@@ -21,6 +29,7 @@ public class HighScore : MonoBehaviour
         private set
         {
             _SCORE = value;
+            PlayerPrefs.SetInt("HighScore", value);
             if (_UI_TEXT != null)
             {
                 _UI_TEXT.text = "High Score: " + value.ToString("#,0");
@@ -31,5 +40,19 @@ public class HighScore : MonoBehaviour
     {
         if (scoreToTry <= SCORE) return; // If scoreToTry is too low, return
         SCORE = scoreToTry;
+    }
+
+    // The following code allows you to easily reset the PlayerPrefs HighScore
+    [Tooltip("Check this box to reset the HighScore in PlayerPrefs")]
+    public bool resetHighScoreNow = false;
+
+    void OnDrawGizmos()
+    {
+        if (resetHighScoreNow)
+        {
+            resetHighScoreNow = false;
+            PlayerPrefs.SetInt("HighScore", 1000);
+            Debug.LogWarning("PlayerPrefs HighScore reset to 1,000.");
+        }
     }
 }
