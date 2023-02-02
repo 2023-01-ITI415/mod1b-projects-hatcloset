69307b5dd738269593002a694594a835fb763462
Tankakuka <officialtankakuka@gmail.com>
2023-02-01 21:54:13 -0500

    Script created for AppleTree

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    // a
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Chance that the AppleTree will change directions
+    public float leftAndRightEdge = 10f;
+
+    // Seconds between Apples instantiations
+    public float appleDropDelay = 1f;
+
+    void Start()
+    {
+        // Start dropping Apples
+//b
+    }
+
+    void Update()
+    {
+        // Basic Movement
+        // b
+        // Changing Direction
+        // b
+    }
+}
