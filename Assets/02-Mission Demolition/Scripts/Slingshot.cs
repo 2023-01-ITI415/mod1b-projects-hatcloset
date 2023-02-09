e4e08983a1aecda52f77c5ebb38b67a65256b976
Tankakuka <officialtankakuka@gmail.com>
2023-02-09 15:16:42 -0500

    LaunchPoint halo added

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
     void OnMouseEnter()
     {
         print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit()
     {
         print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 }
