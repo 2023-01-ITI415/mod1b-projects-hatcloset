ea366527284a7315161f3dbcf03aa57bfe4fe253
Tankakuka <officialtankakuka@gmail.com>
2023-02-09 15:18:54 -0500

    Update Slingshot.cs

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    public GameObject launchPoint;
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
